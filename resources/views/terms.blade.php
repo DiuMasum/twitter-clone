3f265e6ed213fd6096014dc29389482da58ec783
DiuMasum <masum35-577@diu.edu.bd>
2024-03-08 12:18:28 +0600

    connecting to database

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,37 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>terms</h1>
+    <div>
+                    Sure, here's a basic example of website terms and conditions:
+
+        ---
+
+        **Website Terms and Conditions**
+
+        Welcome to [Your Website Name]!
+
+        These terms and conditions outline the rules and regulations for the use of [Your Website Name]'s Website, located at [Your Website URL].
+
+        By accessing this website, we assume you accept these terms and conditions. Do not continue to use [Your Website Name] if you do not agree to take all of the terms and conditions stated on this page.
+
+        The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance, and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of [Your Country/State].
+
+        Cookies
+
+        We employ the use of cookies. By accessing [Your Website Name], you agreed to use cookies in agreement with the [Your Website Name]'s Privacy Policy.
+
+        Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.
+
+        License
+
+        Unless otherwise stated, [Your Website Name] and/or its licensors own the intellectual property rights for all material on [Your Website Name]. All intellectual property rights are reserved. You may access this from [Your Website Name] for your own personal use subjected to restrictions set in these terms and conditions.
+
+        You must not:
+
+        - Republish material from [Your Website Name]
+        - Sell, rent, or sub-license material from [Your Website Name]
+        - Reproduce, duplicate or copy material from [Your Website Name]
+        - Redistribute content from [Your Website Name]
+    </div>
+@endsection
